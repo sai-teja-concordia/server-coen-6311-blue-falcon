a5fc02e564765fc8156b71e7396b997f31a6d0a3
Sai Teja Pamulapati <saiteja.pamulapati@mail.concordia.ca>
2022-10-27 00:48:41 -0400

    restructured pom file

--- src/main/java/com/bluefalcon/project/ProjectApplication.java
+++ src/main/java/com/bluefalcon/project/ProjectApplication.java
@@ -2,10 +2,11 @@ package com.bluefalcon.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.bluefalcon.project")
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ProjectApplication {
 
 	public static void main(String[] args) {
